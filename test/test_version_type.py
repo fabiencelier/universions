7223cd95a4b5c5dcfb6580f39bd2f53abd52d4fe
fabiencelier <fabien.celier@polytechnique.edu>
2019-10-30 22:20:06 +0100

    First implementation for Version

--- test/test_version_type.py
+++ test/test_version_type.py
@@ -1,3 +1,12 @@
-def test_hello_world():
-    hello = "Hello world"
-    assert hello == "Hello world"
+"""Test the version type."""
+
+from universions import Version
+
+def test_define_basic_version():
+    version = Version(1,0,2)
+    print(version)
+    assert version.major == 1
+    assert version.minor == 0
+    assert version.micro == 2
+    assert version.releaselevel == None
+    assert version.serial == None
--- universions/version.py
+++ universions/version.py
@@ -1,13 +1,16 @@
 """Unique format for all the versions."""
 
-from typing import Tuple, Optional
+from typing import Tuple, Optional, NamedTuple
 
 
-class Version(Tuple[int, int, int, str, int]):
+class Version(NamedTuple):
     """Class containing all the version info."""
-
     major: int
-    minor: Optional[int]
-    micro: int
-    releaselevel: str
-    serial: int
+    minor: Optional[int] = None
+    micro: Optional[int] = None
+    releaselevel: Optional[str] = None
+    serial: Optional[int]= None
+
+
+        
+        
