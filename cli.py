d0cdfb8becfa252994f562c1ad1d8d31436aa384
Olivier <kineolyan@protonmail.com>
2019-11-01 22:03:09 +0000

    Remove commented code.

--- cli.py
+++ cli.py
@@ -23,21 +23,11 @@ def main():
     """Main function to be run by the CLI tool."""
     args = parse_args()
 
-    # if len(args) == 0:
-    #     print("No action provided", file=sys.stderr)
-    #     print_help(sys.stderr)
-    #     return 1
-
     tool_name = args.tool
     get_version = TOOLS.get(tool_name)
-
-    # if get_version is None:
-    #     print(f"Unknown tool {tool_name}", file=sys.stderr)
-    #     print_supported_tools(sys.stderr)
-    #     return 1
-
     version = get_version()
     print(version)
+
     return 0
 
 
