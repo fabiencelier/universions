10c9f930299a5e47a67d35a280cfa8228a70d653
fabiencelier <fabien.celier@polytechnique.edu>
2019-11-01 20:22:10 +0100

    Add master_doc = 'index' for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ author = "Fabien Celier"
 # The full version, including alpha/beta/rc tags
 release = "0.1.0"
 
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 
