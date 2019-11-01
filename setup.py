5a44a4451d5f052e51f58ea92d1a82559969b18d
fabiencelier <fabien.celier@polytechnique.edu>
2019-11-01 18:44:25 +0100

    Add description to the pypi package.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 """Setup universions module."""
 
+from os import path
 from setuptools import find_packages, setup
+
+
+def get_long_description():
+    """Read the description from external file."""
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, "README.md"), encoding="utf-8") as file:
+        return file.read()
+
 
 setup(
     name="universions",
-    version="0.1.0-dev021",
+    version="0.1.0-dev022",
     author="Fabien Celier",
     author_email="fabien.celier@polytechnique.org",
     packages=find_packages(exclude=["tests"]),
@@ -12,6 +21,8 @@ setup(
     keywords=["version", "versions", "universal"],
     license="LICENSE.txt",
     description="Get version of other tools.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
