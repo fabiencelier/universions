887c3510d9d1dceebd22f9f1f9c549a1c466b16e
fabiencelier <fabien.celier@polytechnique.edu>
2019-11-01 23:29:00 +0100

    Add github actions for deployment

---
+++ scripts/release.py
@@ -0,0 +1,31 @@
+"""Release script."""
+
+import subprocess
+from os import path
+from pathlib import Path
+from update_version import update_version
+
+PROJECT_DIR = Path(path.abspath(path.dirname(__file__))).parent
+
+update_version()
+
+
+def package():
+    """Package the library."""
+    to_delete = str(PROJECT_DIR / "dist" / "*")
+    print(to_delete)
+    subprocess.check_output(["rm", to_delete, "--force"])
+    subprocess.check_output(["pipenv", "run", "python", "setup.py", "sdist"])
+
+
+def upload_to_pypi():
+    """Upload to Pypi."""
+    to_upload = str(PROJECT_DIR / "dist" / "*")
+    subprocess.check_output(
+        ["pipenv", "run", "twine", "upload", to_upload, "--username", "__token__"]
+    )
+
+
+if __name__ == "__main__":
+    package()
+    upload_to_pypi()
