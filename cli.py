ff35461cd3dd0b5209ac574e9ecadc5e12b43cbd
Kineolyan <kineolyan@gmail.com>
2019-11-02 14:21:47 +0100

    Adding support for ourselves.

--- cli.py
+++ cli.py
@@ -1,11 +1,31 @@
 """Main file to call universions from the CLI."""
 
 import argparse
+import pkg_resources
 
+import universions
 import universions.java as uj
 import universions.node as un
 
-TOOLS = {"java": uj.get_java_versions, "node": un.get_node_versions}
+
+def get_self_version():
+    """
+    Gets the version of this exact module.
+    From the advised alternatives in
+    https://packaging.python.org/guides/single-sourcing-package-version/, this reads from setup.py.
+    But there is a little problem: for a version such as 0.1.0-dev21, the attribute version returns
+    '0.1.0.dev21'. This issue is that the '-' is replaced by a '.', which makes it an invalid semver
+    """
+    raw_version = pkg_resources.require("universions")[0].version
+    version = raw_version.replace(".dev", "-dev")
+    return universions.parse_semver(version)
+
+
+TOOLS = {
+    "java": uj.get_java_versions,
+    "node": un.get_node_versions,
+    "universions": get_self_version,
+}
 
 
 def parse_args():
@@ -17,9 +37,10 @@ def parse_args():
         choices=list(TOOLS.keys()),
     )
     parser.add_argument(
-        '-v', "--verbosity",
+        "-v",
+        "--verbosity",
         help="Configures the verbosity level of the printed version",
-        action='count',
+        action="count",
         default=0,
     )
     return parser.parse_args()
@@ -32,6 +53,7 @@ def print_version(version, verbosity):
     if 1 <= verbosity < 3:
         return f"{version.major}.{version.minor}.{version.patch}"
     return str(version)
+
 
 def main():
     """Main function to be run by the CLI tool."""
