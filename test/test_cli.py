4904951de17f8ca598d7c232186824ada7071f58
Fabien Celier <fabien.celier@polytechnique.edu>
2019-11-06 22:22:46 +0100

    Add Macos to CI tests (#35)

--- test/test_cli.py
+++ test/test_cli.py
@@ -9,8 +9,3 @@ def test_cli_help():
     assert str(output, encoding="utf-8").startswith("usage: universions")
     output = check_output(["universions", "--help"])
     assert str(output, encoding="utf-8").startswith("usage: universions")
-
-
-def test_cli_help():
-    output = check_output(["universions", "java"])
-    assert str(output, encoding="utf-8").startswith("usage: universions")
