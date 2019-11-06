d5c89d90cb17ade13e7789d5fdb96b31525f1954
Fabien Celier <fabien.celier@polytechnique.edu>
2019-11-06 23:46:16 +0100

    Remove pkg_resources dependecy, add CLI verbosity test (#40)

--- test/test_cli.py
+++ test/test_cli.py
@@ -2,6 +2,8 @@
 
 from subprocess import check_output
 
+import universions
+
 
 def test_cli_help():
     """Check 'universions -h'."""
@@ -9,3 +11,19 @@ def test_cli_help():
     assert str(output, encoding="utf-8").startswith("usage: universions")
     output = check_output(["universions", "--help"])
     assert str(output, encoding="utf-8").startswith("usage: universions")
+
+
+def test_cli_universions():
+    """Test 'universions universions'."""
+    output = check_output(["universions", "universions"])
+    expected = ".".join(universions.__version__.split(".")[0:2])
+    assert str(output, encoding="utf-8").strip() == expected
+
+
+def test_cli_universions_verbosity():
+    """Test 'universions universions'."""
+    expected = ".".join(universions.__version__.split(".")[0:3])
+    output = check_output(["universions", "universions", "-v"])
+    assert str(output, encoding="utf-8").strip() == expected
+    output = check_output(["universions", "universions", "-vv"])
+    assert str(output, encoding="utf-8").strip() == expected
