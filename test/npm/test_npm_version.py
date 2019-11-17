c286c78ff77d5bcef4227305b6c25fbdd5c1fb45
Kineolyan <kineolyan@gmail.com>
2019-11-17 22:05:41 +0100

    Push tests.

--- test/npm/test_npm_version.py
+++
@@ -1,7 +0,0 @@
-"""Test the parsing of npm version."""
-
-from universions.npm import get_npm_versions
-
-
-def test_node():
-    print(get_npm_versions())
---
+++ test/test_npm_version.py
@@ -0,0 +1,34 @@
+"""Test the npm version."""
+
+from pathlib import Path
+
+import pytest
+
+from universions import Version
+from universions.npm import get_npm_versions
+
+from .mock_popen import BasicMockedPopen
+
+MAC_OUT = b"5.0.4"
+LINUX_OUT = b"6.4.1"
+
+VERSIONS = [
+    pytest.param(MAC_OUT, Version(5, 0, 4)),
+    pytest.param(LINUX_OUT, Version(6, 4, 1)),
+]
+
+
+class TestNpm(BasicMockedPopen):
+    """Test the Npm version."""
+
+    @pytest.mark.parametrize("command,version", VERSIONS)
+    def test_default_npm_version(self, command: bytes, version: Version):
+        """Test that the correct version is returned."""
+        self.popen.set_command("npm --version", stderr=command)
+        assert get_npm_versions() == version
+
+    def test_npm_version_with_path(self):
+        """Test that the correct version is returned when specifying path."""
+        path = str(Path.home() / "npm-here" / "bin" / "npm.bin")
+        self.popen.set_command(f"{path} --version", stderr=LINUX_OUT)
+        assert get_npm_versions(npm_path=path) == version
