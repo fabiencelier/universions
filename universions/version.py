1cf39baa05465cdfe912ca9f2631aa7feeb850ea
Kineolyan <kineolyan@gmail.com>
2019-11-24 16:44:17 +0100

    (WIP) More tests passing.

--- universions/version.py
+++ universions/version.py
@@ -10,15 +10,27 @@ from .error import InvalidVersionFormatError
 def lt_version_number(a, b):
     if a == b:
         return None
+    return a < b
+
+
+def lt_version_option(a, b):
+    if a == b:
+        return None
     if a is None:
-        return True
-    if b is None:
         return False
+    if b is None:
+        return True
 
     return a < b
 
 
-COMPARE_ORDER = [lambda a: a.major, lambda a: a.minor, lambda a: a.patch]
+def option_rank(version):
+    return (10 if version.prerelease is None else 0) + (
+        1 if version.build is None else 0
+    )
+
+
+COMPARE_ORDER = [lambda a: a.major, lambda a: a.minor, lambda a: a.patch, option_rank]
 COMPARE_DETAILS = [lambda a: a.prerelease, lambda a: a.build]
 
 
@@ -29,10 +41,11 @@ class Version:
     def __init__(
         self,
         major: int,
-    minor: Optional[int] = None,
-    patch: Optional[int] = None,
-    prerelease: Optional[str] = None,
-    build: Optional[str] = None):
+        minor: Optional[int] = None,
+        patch: Optional[int] = None,
+        prerelease: Optional[str] = None,
+        build: Optional[str] = None,
+    ):
         self.major = major
         self.minor = minor or 0
         self.patch = patch or 0
@@ -43,8 +56,13 @@ class Version:
         if not isinstance(other, Version):
             return NotImplemented
 
-        return self.major == other.major and self.minor == other.minor and self.patch == other.patch and self.prerelease == other.prerelease and self.build == other.build
-
+        return (
+            self.major == other.major
+            and self.minor == other.minor
+            and self.patch == other.patch
+            and self.prerelease == other.prerelease
+            and self.build == other.build
+        )
 
     def __lt__(self, other):
         if not isinstance(other, Version):
@@ -56,11 +74,17 @@ class Version:
                 return result
 
         for attribute in COMPARE_DETAILS:
-            result = lt_version_number(attribute(self), attribute(other))
+            result = lt_version_option(attribute(self), attribute(other))
             if result is not None:
-                return not result
+                return result
 
         return False
+
+    def __str__(self):
+        return f"Version(major={self.major}, minor={self.minor}, patch={self.patch}, prerelease={self.prerelease}, build={self.build})"
+
+    def __repr__(self):
+        return Version.__str__(self)
 
 
 _REGEX = re.compile(
